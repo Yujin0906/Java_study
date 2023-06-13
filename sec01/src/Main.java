79c70150caca5261ef7ad41293b78194bef93342
Yujin Shin <dbwls020906@gmail.com>
2023-06-13 13:53:24 +0900

    2023.06.12

---
+++ sec01/src/Main.java
@@ -0,0 +1,54 @@
+public class Main {
+    public static void main(String[] args)
+    {
+//        System.out.print("Hello"); // 문자열(string), 괄호 안에 있는 내용을 출력함
+//        System.out.print("Hello");
+//        System.out.print("Hello");
+
+        System.out.println("Hello Java!!"); // sout(자동완성), println(print line) 줄 단위 출력
+        System.out.println(); // 줄 바꿈
+        System.out.println("Hello Java!!");
+        System.out.println();
+        System.out.println("Hello Java!!");
+//        System.out.println(3*4);
+
+        // System.out.printf(""); // souf(자동완성)
+
+        // 변수 (Variable)
+
+        int number = 0;
+        int numberRandom = 0;
+        int number_random = 0;
+        int int_number = 0;
+
+        // 정수형 데이터 타입의 변수
+        byte a = 1;   // 1byte(-128 ~ +127)
+        char b = 2;   // 2byte
+        short c = 3;  // 2byte
+        int d = 5;    // 4byte
+        long e = 10;  // 8byte
+
+        // 실수형 데이터 타입의 변수
+        float f = 0.1f;    // 4byte
+        double g = 0.001d; // 8byte
+
+        // 논리형 데이터 타입의 변수
+        boolean h = true;
+
+        // 문자열(형) 데이터 타입의 변수
+        String i = "Hello Java";
+
+        char ch = 'A';
+        char A = 66;
+
+        String str = "Hello JavA";
+
+
+        System.out.printf("number = %d\n", c);
+        System.out.printf("Real Number = %f\n",123.45f);
+        System.out.printf("문자 = %c",ch);
+
+
+
+    }
+}
